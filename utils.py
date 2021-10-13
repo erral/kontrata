e9e20e7e30bfd2a78e34e2c76805ac02433d5beb
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-10-13 09:49:01 +0200

    log the first parameter of the method

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ def print_progress(func):
     """ print the progress of the func running"""
 
     def wrapper(*args, **kwargs):
-        print("Running {}...".format(func.__name__))
+        print("Running {} ({})...".format(func.__name__, args and args[0] or ""))
         result = func(*args, **kwargs)
         print("Running {} done".format(func.__name__))
         return result
