438110908cf8fca8039b9b47a344a8986eef627a
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-10-17 20:36:52 +0200

    add tests for old xml float values

--- test_step_02_process_contracts.py
+++ test_step_02_process_contracts.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from step_02_process_contracts import clean_float_value
+from step_02_process_contracts import clean_float_value_old_xml
 
 import unittest
 
 
-class TestProcessContracts(unittest.TestCase):
+class TestCleanFloatValue(unittest.TestCase):
     def test_dot_as_decimal(self):
         value = "202.49"
         new_value = clean_float_value(value)
@@ -39,6 +40,28 @@ class TestProcessContracts(unittest.TestCase):
         value = "4.268.35"
         new_value = clean_float_value(value)
         self.assertEqual(new_value, 4268.35)
+
+
+class TestCleanFloatValueOldXML(unittest.TestCase):
+    def test_million_with_decimals(self):
+        value = "1.216.511,05"
+        new_value = clean_float_value_old_xml(value)
+        self.assertEqual(new_value, 1216511.05)
+
+    def test_with_one_decimal_number(self):
+        value = "2.076,4"
+        new_value = clean_float_value_old_xml(value)
+        self.assertEqual(new_value, 2076.4)
+
+    def test_integer_no_thousands(self):
+        value = "275"
+        new_value = clean_float_value_old_xml(value)
+        self.assertEqual(new_value, 275.0)
+
+    def test_integer_thousands_separator(self):
+        value = "1.025"
+        new_value = clean_float_value_old_xml(value)
+        self.assertEqual(new_value, 1025.0)
 
 
 if __name__ == "__main__":
